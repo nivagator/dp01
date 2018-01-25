d2116cf1e57d82af3203f9b1cef1e4f20681281c
Gavin Greer <gavingreer@mac.com>
2018-01-25 14:08:13 -0600

    create detail view

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import post_model_list_view
+from .views import (
+    post_model_list_view, 
+    post_model_detail_view,
+)
 
 urlpatterns = [
     path('', post_model_list_view, name='list'),
-    
+    path('1/', post_model_detail_view, name='detail'),
     # path('admin/', admin.site.urls),
     # path('', home, name='home'),
     # path('redirect/', redirect_somewhere, name='redir')
