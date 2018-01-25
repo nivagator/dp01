0ca35579fa4524ff94398e1f1e16fb6b74494935
Gavin Greer <gavingreer@mac.com>
2018-01-25 14:08:02 -0600

    add login required to list view

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
-
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 
 from .models import PostModel
 
@@ -9,17 +9,41 @@ from .models import PostModel
 # CREATE 
 
 # RETRIEVE 
+def post_model_detail_view(request):
+    obj = get_object_or_404(PostModel, id=1)
+    context = {
+        "object": obj,
+        }
+    template = "blog/detail-view.html"
+    return render(request, template, context)
 
 # UPDATE 
 
 # DELETE
 
 # LIST
+
 def post_model_list_view(request):
     qs = PostModel.objects.all()
-    print(qs)
-    template_path = "list-view.html"
-    context = {}
-    return render(request, template_path, context)
-    # return HttpResponse("some data")
+    context = {
+            "object_list": qs,
+        }
+    template = "blog/list-view.html"
+    return render(request, template, context)
 
+@login_required(login_url='/login')
+def login_required_view(request):
+    print(request.user)
+    qs = PostModel.objects.all()
+    context = {
+            "object_list": qs,
+        }
+    if request.user.is_authenticated:
+        print("logged in")
+        template = "blog/list-view.html"
+    else:
+        print("not logged in")
+        template = "blog/list-view-public.html"
+        raise Http404
+        # return HttpResponseRedirect("/login")
+    return render(request, template, context)
